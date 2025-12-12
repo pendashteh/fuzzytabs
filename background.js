fe5711d329fe5cfa011bae458310e9c802a0927d
Alexar <github@alexar.me>
2025-12-12 19:28:42 +1100

    Goes back to the same tab after pressing the hotkey again

--- background.js
+++ background.js
@@ -1,18 +1,33 @@
 // Open tab finder when extension icon is clicked
 chrome.action.onClicked.addListener(() => {
-  chrome.tabs.create({
-    url: chrome.runtime.getURL('popup.html')
-  });
+  openOrFocusTabFinder();
 });
 
 // Open tab finder with keyboard shortcut
 chrome.commands.onCommand.addListener((command) => {
   if (command === 'open-tab-finder') {
-    chrome.tabs.create({
-      url: chrome.runtime.getURL('popup.html')
-    });
+    openOrFocusTabFinder();
   }
 });
+
+// Open or focus existing tab finder
+function openOrFocusTabFinder() {
+  const finderUrl = chrome.runtime.getURL('popup.html');
+  
+  // Check if tab finder is already open
+  chrome.tabs.query({}, (tabs) => {
+    const existingTab = tabs.find(tab => tab.url === finderUrl);
+
+    if (existingTab) {
+      // Tab finder already open - focus it
+      chrome.tabs.update(existingTab.id, { active: true });
+      chrome.windows.update(existingTab.windowId, { focused: true });
+    } else {
+      // Tab finder not open - create new tab
+      chrome.tabs.create({ url: finderUrl });
+    }
+  });
+}
 
 // Track when tabs are created
 chrome.tabs.onCreated.addListener((tab) => {
